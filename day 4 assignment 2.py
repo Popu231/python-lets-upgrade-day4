13aad9071f818b9585b1d668c09d95bfb839eb66
Popu231 <43511564+Popu231@users.noreply.github.com>
2020-07-23 21:55:47 +0530

    Add files via upload
    python-lets-upgrade-day4

---
+++ day 4 assignment 2.py
@@ -0,0 +1,7 @@
+str1 = "I live in India, India is a great country since 1947"
+
+print(str1.lower())
+
+
+print(str1.upper())
+
---
+++ day 4 assingment 1.py
@@ -0,0 +1,8 @@
+str1 = "I live in India, India is a great country"
+
+print(str1.count("India"))
+
+
+print(str1.count("India",0,15))
+
+print(str1.count("i"))
